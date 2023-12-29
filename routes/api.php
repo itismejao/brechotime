3bf764bd0f4ef87aa3cb457635d53229353eeac2
joao.neto@etus.com.br <joao.neto@etus.com.br>
2023-12-29 10:20:31 -0300

    configurações firebase

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,41 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use Kreait\Firebase\Exception\Auth\FailedToVerifyToken;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+/* test hello */
+Route::get('/hello', function () {
+    return 'hello';
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/* test api */
+Route::post('/checktoken', function (Request $request) {
+    //echo $request;
+    $response = (object) [];
+    $token = $request->bearerToken(); 
+    $response->token = $token;
+
+    $auth = app('firebase.auth');
+
+    try {
+        $verifiedIdToken = $auth->verifyIdToken($token);
+        //echo var_dump($verifiedIdToken);
+    } catch (FailedToVerifyToken $e) {
+        echo 'The token is invalid: '.$e->getMessage();
+    }
+
+    /* check token payload */
+    $email = $verifiedIdToken->claims()->get('email');
+    $response->payload_email = $email;
+    $uid = $verifiedIdToken->claims()->get('sub');
+    $response->payload_uid = $uid;
+
+    /* check authenticated user */
+    $user = $auth->getUser($uid);
+    $response->authenticated_user = $user;
+
+    return response()->json($response, 200);
+
 });
